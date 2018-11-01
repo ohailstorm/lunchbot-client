39755c1acc7146405209600edc110b2bd95f33ce
Oscar Hallström <oscarhstrom@gmail.com>
2018-11-01 23:48:18 +0100

    add react router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/list/">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={App} />
+      <Route path="/list/" component={App} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 registerServiceWorker();
